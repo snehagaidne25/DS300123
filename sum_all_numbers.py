82f04df7d7c6796b8f6e3fdb3a1c057328d4413e
snehagaidne25 <124501461+snehagaidne25@users.noreply.github.com>
2023-03-10 11:23:14 +0530

    Add files via upload

---
+++ sum_all_numbers.py
@@ -0,0 +1,16 @@
+#python function to sum all no in list
+def sum_of_list(a):
+    total=0
+    for value in a:
+      total= total + value
+    return total
+
+my_list = [8,2,3,0,7]
+print ("the sum of my_list is", sum_of_list(my_list))
+
+        
+    
+
+
+
+
